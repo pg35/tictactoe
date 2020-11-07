c962c8a6ffa816c457b43f58d211fb672219af9a
pg35 <pg35@users.noreply.github.com>
2020-11-08 03:43:17 +0500

    handled game ending - victory, draw and score. control button styled

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,46 +6,136 @@ import Board from "./Board";
 
 import { getPlayerSymbol } from "../utils/common";
 
+const GameStatus = Object.freeze({
+  READY: Symbol("READY"),
+  STARTED: Symbol("STARTED"),
+  PAUSED: Symbol("PAUSED"),
+  FINISHED: Symbol("FINISHED")
+});
+
 class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       turn: 0,
       moves: {},
-      score: [0, 0]
+      score: [0, 0],
+      status: GameStatus.READY
     };
     this.handleCellClick = this.handleCellClick.bind(this);
+    this.changeStatus = this.changeStatus.bind(this);
   }
   componentDidMount() {}
 
   getNextTurn(turn) {
-    return !turn;
+    return turn ? 0 : 1;
+  }
+
+  getNextStatus(status) {
+    switch (status) {
+      case GameStatus.READY:
+        return GameStatus.STARTED;
+      case GameStatus.STARTED:
+        return GameStatus.PAUSED;
+      case GameStatus.PAUSED:
+        return GameStatus.STARTED;
+      case GameStatus.FINISHED:
+        return GameStatus.STARTED;
+      default:
+        throw new Error("Invalid status");
+    }
+  }
+  getStatusLabel(status) {
+    switch (status) {
+      case GameStatus.READY:
+        return "Play";
+      case GameStatus.STARTED:
+        return "Pause";
+      case GameStatus.PAUSED:
+        return "Resume";
+      case GameStatus.FINISHED:
+        return "Replay";
+      default:
+        throw new Error("Invalid status");
+    }
+  }
+  changeStatus(status) {
+    if (GameStatus.FINISHED === this.state.status) {
+      this.setState({
+        moves: {}
+      });
+    }
+    this.setState((state) => ({
+      status: this.getNextStatus(state.status)
+    }));
+  }
+  isGameFinished(rows, cols, cells) {
+    let i, j;
+    for (i = 0; i < rows; ++i) {
+      const v1 = cells[i][0];
+      const v2 = cells[0][i];
+      for (j = 1; j < cols && "" !== v1 && v1 === cells[i][j]; ++j);
+      if (j === cols) return true;
+      for (j = 1; j < rows && "" !== v2 && v2 === cells[j][i]; ++j);
+      if (j === rows) return true;
+    }
+    const v1 = cells[0][0];
+    for (i = 1; i < rows && "" !== v1 && v1 === cells[i][i]; ++i);
+    if (i === rows) return true;
+    const v2 = cells[0][cols - 1];
+    for (
+      i = 1, j = cols - 2;
+      i < rows && "" !== v2 && v2 === cells[i][j];
+      ++i, --j
+    );
+    if (i === rows) return true;
+    return false;
   }
   handleCellClick(r, c) {
     const { size } = this.props;
-    const { turn } = this.state;
-    this.setState((state) => ({
-      turn: this.getNextTurn(state.turn),
-      moves: { ...state.moves, [r * size + c]: [turn, r, c] }
-    }));
+    const { turn, moves, score, status } = this.state;
+    if (GameStatus.STARTED !== status) return;
+
+    const newMoves = { ...moves, [r * size + c]: [turn, r, c] };
+    const cells = this.prepareCells(size, size, newMoves);
+    const isDraw = size * size === Object.keys(newMoves).length;
+    const isFinished = isDraw || this.isGameFinished(size, size, cells);
+
+    const newState = {
+      moves: newMoves,
+      status: isFinished ? GameStatus.FINISHED : status
+    };
+    if (!isFinished) {
+      newState.turn = this.getNextTurn(turn);
+    } else if (!isDraw) {
+      ++score[turn];
+      newState.score = score;
+    }
+    this.setState(newState);
   }
-  prepareCells(rows, cols, cells) {
+  prepareCells(rows, cols, moves) {
     const arr = new Array(rows);
     for (let i = 0; i < rows; ++i) arr[i] = new Array(cols);
     for (let i = 0; i < rows; ++i) {
       for (let j = 0; j < cols; ++j) {
         const k = i * rows + j;
-        arr[i][j] = cells[k] ? getPlayerSymbol(cells[k][0]) : "";
+        arr[i][j] = moves[k] ? moves[k][0] : "";
       }
     }
     return arr;
   }
   render() {
     const { size } = this.props;
-    const { turn, moves, score } = this.state;
+    const { turn, moves, score, status } = this.state;
     const [rows, cols] = [size, size];
 
-    const cells = this.prepareCells(rows, cols, moves);
+    const cells = this.prepareCells(rows, cols, moves).map((row, i) =>
+      row.map((v, j) => {
+        const k = i * rows + j;
+        return moves[k] ? getPlayerSymbol(moves[k][0]) : v;
+      })
+    );
+
     return (
       <div className="game">
         <Score score={score} />
@@ -56,6 +146,9 @@ class Game extends React.Component {
           cells={cells}
           onCellClick={this.handleCellClick}
         />
+        <button className="game-control" onClick={this.changeStatus}>
+          {this.getStatusLabel(status)}
+        </button>
       </div>
     );
   }
